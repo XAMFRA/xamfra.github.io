d9c43006d1e2a08f2b063a1bac0cc0b23347e9fb
AhmedAbdelaziz.Reda <87047408+XAMFRA@users.noreply.github.com>
2021-10-27 00:09:20 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 function OPENNAV() {
-	document.getElementById("XARNAV").style.width = "50%";
+	document.getElementById("XARNAV").style.width = "200px";
 }
 function CLOSENAV() {
 	document.getElementById("XARNAV").style.width = "0";
 }
+/*
+XAR JS CODE
+*/
