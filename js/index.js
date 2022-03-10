4106563450d40609960ce49bf8dd92be1f3a37e5
AhmedAbdelaziz.Reda <87047408+XAMFRA@users.noreply.github.com>
2022-03-10 22:43:41 +0200

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function OPENNAV() {
-	document.getElementById("XARNAV").style.width = "200px";
-}
-function CLOSENAV() {
-	document.getElementById("XARNAV").style.width = "0";
-}
-/*
-XAR JS CODE
-*/
